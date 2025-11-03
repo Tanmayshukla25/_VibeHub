be6391186f83dcc29d503bb944e80922100fefe7
Tanmayshukla25 <tanmayshukla252@gmail.com>
2025-11-03 14:07:03 +0530

    CorsError

--- Backend/index.js
+++ Backend/index.js
@@ -8,8 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+// const allowedOrigins = [
+//   process.env.FRONTEND_URL, 
+// ];
 const allowedOrigins = [
-  process.env.FRONTEND_URL, 
+  process.env.FRONTEND_URL || "https://vibehub-1-65d1.onrender.com",
+  "http://localhost:5173", 
 ];
 
 const localhostRegex = /^http:\/\/localhost(:\d+)?$/;
